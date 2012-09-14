70018ee44093f232525350525d640ffad396fc03
Johnny Jung <me@johnnyjung.com>
2012-09-13 20:52:15 -0700

    Revert "Revert "Lab0 Done (9-8)""
    This reverts commit 38e2f5c3a162cfee55e99d881ddfad81c84b5c79.

---
+++ workspace/_include/52259_asm_template/MCF52259_Console_Internal_RAM/Sources/console_io_cf.d
@@ -0,0 +1,22 @@
+Sources/console_io_cf.obj: C:\Users\Mark\workspace\52259_asm_template\Sources\console_io_cf.c \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\lib_ewl.prefix \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\common.prefix \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_c_version.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\ansi_parms.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\ewlGlobals.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\coldfire\ansi_prefix.CF.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\os_enum.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\console_io.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\stdio.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\cstdio \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\stddef.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\cstddef \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\null.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\size_t.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\wchar_t.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_lib_ext1.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\ewl_rsize_t.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\eof.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\va_list.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\file_struc.h \
+	C:\Program\ Files\ (x86)\Freescale\CW\ MCU\ v10.0\MCU\ColdFire_Support\ewl\EWL_C\include\stdio_api.h 
