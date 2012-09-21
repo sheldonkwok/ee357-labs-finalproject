8bcfd20ba0e30df3b32d0f2d7c0da5d4d2ff103e
Johnny Jung <me@johnnyjung.com>
2012-09-21 16:10:26 -0700

    Turned in Lab1 / Some Lab

--- workspace/ee357_lab1_io_assem/Sources/main.c
+++ workspace/ee357_lab1_io_assem/Sources/main.c
@@ -21,57 +21,46 @@ int main(void)
 	#endif
 	
 	asm{
-		// Switch input
+		// Setup for Switch / LED
 		move.l #0x0f,d0
-		move.b d0,0x40100074 	// 00001111 into PDDPAR
-		move.b d0,0x4010002C	// 00001111 into DDRDD
-		move.b 0x40100044,d2	// move switch input to D2
-		eori.l #0xf0,d2			// flip bits
-		lsr.l #4,d2				// get rid of 4LSB bits 
-		
-		// LED output
-		move.b d2,0x4010000F	// D2 into PORTTC
+		move.b d0,0x40100074 	// 00001111 into PDDPAR	(Switch)
+		move.b d0,0x4010002C	// 00001111 into DDRDD	(Switch)
+		move.b d0,0x40100027	// 00001111 into DDRTC	(LED)
 		move.l #0,d0
-		move.b d0,0x4010006F 	// 00000000 into PTCPAR
-		move.l #0x0f,d0			
-		move.b d0,0x40100027	// 00001111 into DDRTC
+		move.b d0,0x4010006F 	// 00000000 into PTCPAR	(LED)
+		
+		// Actual I/O
+		move.b 0x40100044,d1	// move switch input to D1
+		eori.l #0xf0,d1			// flip 4MSB bits -> can also do not.l d1
+		lsr.l #4,d1				// get rid of 4LSB bits 
+		move.b d1,0x4010000F	// D1 into PORTTC
 		
 		// Decrementing
-	L2: subq.l #1, d2
-		andi.l #0x0f, d2
-		move.b d2,0x4010000F
-		bra L2
-		
-	/*	Existing Incrementing
-		clr.l d1
-	L1:	subq.l #1, d1
+	L1: subq.l #1, d1
 		andi.l #0x0f, d1
 		move.b d1,0x4010000F
 		bra L1
-	*/
 		
-		
-	/*	ARI w/ Displacement
-	 * 
-		move.l #0x40100000,A1
-		move.l #0x0f,d0
-		move.b d0,(0x74,A1)
-		move.b d0,(0x2c,A1)
-		eori.l #0xf0,d2
-		lsr.l #4,d2
-		
-		move.b d2,(0x0F,A1)
-		move.l #0, d0
-		move.b d0,(0x6F,A1)
-		move.l #0x0f,d0
-		move.b d0,(0x27,A1)
-		
-	L2: subq.l #1, d2
-		andi.l #0x0f, d2
-		move.b d2,(0x0F,A1)
-		bra L2
-	*/
 
+		/*	ARI w/ Displacement 
+		move.l #0x40100000,A1	// 0x40100000 into A1
+		move.l #0x0f,d0			
+		move.b d0,0x74(A1)		// 00001111 into PDDPAR
+		move.b d0,0x2c(A1)		// 00001111 into DDRDD
+		move.b d0,0x27(A1)		// 00001111 into DDRTC
+		move.l #0,d0
+		move.b d0,0x6F(A1)		// 00000000 into PTCPAR
+		move.b 0x44(A1),d1		// move switch input to D2
+		eori.l #0xf0,d1
+		lsr.l #4,d1
+		move.b d1,0x0F(A1)		// D2 into PORTTC
+		
+		// Decrementing
+	L2: subq.l #1, d1
+		andi.l #0x0f, d1
+		move.b d1,0x0F(A1)
+		bra L2
+		*/
 	}
 	
 	for(;;) {	   
--- workspace/lab0/Sources/main.c
+++ workspace/lab0/Sources/main.c
@@ -19,6 +19,36 @@ int main(void)
 #if (CONSOLE_IO_SUPPORT || ENABLE_UART_SUPPORT)
 	printf("Hello World in C++ from MCF52259 derivative on MCF52259 board\n\r");
 #endif
+	
+
+	
+	asm {
+		
+
+		/* Question #3
+		move.l #0xabcd0123,D0
+		move.l #0x12345678,D1
+		move.l D1,0x20003004
+		move.w 0x20003004, D0
+		move.w D0,0x20003006
+		move.b D0,0x20003004
+		move.w 0x20003004, D0
+		move.b 0x20003007, D0
+		move.w D0,0x20003004
+		rts
+		*/
+		
+		/* Question #7 
+		MOVE.W #0x0000FFFF,D0 // I0
+		MOVE.L 0x12345678,D2 // I1
+		ADD.L -(A2),D2 // I2
+		MOVE.W 7(A2),D1 // I3
+		MOVE.W (A2)+, -4(A1,D2*2) // I4
+		*/
+		
+	}
+	
+	
 	for(;;) {	   
 	   	counter++;
 	}
