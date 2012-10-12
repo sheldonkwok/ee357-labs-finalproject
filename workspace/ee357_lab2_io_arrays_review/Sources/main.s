687c0277de8a783fb32d20ebff420676af801ede
Johnny Jung <me@johnnyjung.com>
2012-10-11 22:15:46 -0700

    Finished Lab2
    Done w/ lab 2 + review question

--- workspace/ee357_lab2_io_arrays_review/Sources/main.s
+++ workspace/ee357_lab2_io_arrays_review/Sources/main.s
@@ -1,5 +1,5 @@
 /*
- * EE357 - Lab 2
+ * EE357 - Lab 2 Review Question
  * Johnny Jung / Sheldon Kwok
  *
  */
@@ -14,13 +14,15 @@ arr1:	.space NSAMP
 _main:
 main:		
 		// a1 - pointer for arr1
+		// d4 - scaled index counter
+		
 		// d0 - use for LED/Switch registers
 		// d1 - temporarily hold 1st number and later average
 		// d2 - temporarily hold 2nd number
 		// d3 - hold loop count for waiting
 		
-		// Setting Up arr1 Pointer
-		movea.l #arr1,a1
+		movea.l #arr1,a1		// Setting Up arr1 Pointer
+		move.l #0,d4 			// Setting Up Scaled Index to 0
 		
 		// Initialization of LED / Switch
 		move.l #0x0f,d0
@@ -36,8 +38,8 @@ main:
 		lsr.l #4,d1				// Get rid of 4LSB bits of 1st #
 		andi.l #0x0f,d1			// Clear 1st # except for 4 LSB
 		
-		move.b d1,(a1)			// Move 1st # to arr1[0]
-		move.b (a1),0x4010000F 	// Display 1st # - arr1[0] into PORTTC (LED)
+		move.b d1,0(a1,d4*1)	// Move 1st # to arr1[0]
+		move.b d1,0x4010000F 	// Display 1st # - Move 1st # into PORTTC (LED)
 		clr.l d1				// Clear D1
 		
 		// Read and Loop Until Switch Input Changes
@@ -46,25 +48,25 @@ LOOP1:	move.b 0x40100044,d2	// Read 2nd # into D2
 		lsr.l #4,d2				// Get rid of 4LSB bits of 2nd #
 		andi.l #0x0f,d2			// Clear 2nd # except for 4 LSB
 		
-		move.b (a1),d1 			// Move 1st # to D1 - doing this as CMP is a longword operation
+		move.b 0(a1,d4*1),d1 	// Move 1st # to D1 - doing this as CMP is a longword operation
 		CMP.l d2,d1				// Compare 1st # to 2nd # -> check if equal
 		BEQ LOOP1				// Loop back if switch input is the same
 		
-		move.b (a1)+,d1 		// Increment to arr1[1] -> can't do this in the loop
+		add.l #1,d4				// Increment Scaled Index to 1
 		clr.l d1				// Clear D1
 				
 		// Store, Display 2nd Number
-		move.b d2,(a1)			// Move 2nd # to arr1[1]
-		move.b (a1),0x4010000F	// Display 2nd # - arr[1] into PORTTC (LED)
+		move.b d2,0(a1,d4*1)	// Move 2nd # to arr1[1]
+		move.b d2,0x4010000F	// Display 2nd # - Move 2nd # into PORTTC (LED)
 		clr.l d2				// Clear D2
 		
 		// Calculate Average Number
-		move.b -(a1),d1			// Decrement to arr[0] + Move arr[0] to D1
-		move.b (a1)+,d1			// Move arr[0] to D1 (again) + Increment to arr[1]
-		move.b (a1)+,d2			// Move arr[1] to D2 + Increment to arr[2]
+		move.b -1(a1,d4*1),d1	// Move arr[0] to D1
+		move.b 0(a1,d4*1),d2	// Move arr[1] to D2
 		add.l d2,d1				// Add two numbers together - doing this as ADD is a longword operation
 		lsr.l #1,d1				// "Divide" by 2 to get Average # in D1
-		move.b d1,(a1)			// Move Average # in D1 to arr1[2]
+		add.l #1,d4				// Increment Scaled Index to 2
+		move.b d1,0(a1,d4*1)	// Move Average # in D1 to arr1[2]
 	
 		// Loop To Wait (Total) 10 Seconds
 		move.l #33333333,d3
@@ -75,7 +77,8 @@ LOOP2: 	SUBI.l #1,D3
 		
 		/* Blinking Average Loop */ 
 		// Displaying Average #
-AGAIN:	move.b (a1),0x4010000F 	// Display Average # - arr[2] into PORTTC (LED)
+AGAIN:	move.b 0(a1,d4*1),d1
+		move.b d1,0x4010000F 	// Display Average # - arr[2] into PORTTC (LED)
 		
 		// Loop to Wait 1 Second (on)
 		move.l #3333333,d3
